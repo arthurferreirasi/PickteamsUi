594bf1c81fe8b5ff4c0b909daf45910e2f7a3bce
arthurferreirasi <arthursiqueiraferreira@outlook.com>
2024-10-26 16:11:26 -0300

    config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PickTeamsComponent } from './pick-teams/pick-teams.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'pick-teams', pathMatch: 'full'},
+    { path: 'pick-teams', component: PickTeamsComponent }
+];
