98d3a7a289db31a4295576081bccf69ee492ecc2
arthurferreirasi <arthursiqueiraferreira@outlook.com>
2024-11-03 18:53:16 -0300

    added toogle for itens

--- src/app/select-seed-players-modal/select-seed-players-modal.component.ts
+++ src/app/select-seed-players-modal/select-seed-players-modal.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { Player } from '../models/player';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-select-seed-players-modal',
   standalone: true,
-  imports: [MatDialogModule, CommonModule],
+  imports: [MatDialogModule, CommonModule, MatSlideToggleModule],
   templateUrl: './select-seed-players-modal.component.html',
   styleUrl: './select-seed-players-modal.component.css'
 })
